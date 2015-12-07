852c8cf3f236cac85afda01d303bb1bd8323b562
Nicolas GUILLAUD <nicolas@apiwork.com>
2015-12-07 09:24:36 +0100

    Remove comments on launcher

--- launcher.php
+++ launcher.php
@@ -2,20 +2,20 @@
 /* Load Referential */
 require_once('lib/ApimoApiReferential.php');
 $api = new Apimo_Api_Referential();
-//$report = $api->update();
-//print_r($report);
+$report = $api->update();
+print_r($report);
 
 /* Load Agencies */
 require_once('lib/ApimoApiAgencies.php');
 $api = new Apimo_Api_Agencies();
-//$report = $api->update();
-//print_r($report);
+$report = $api->update();
+print_r($report);
 
 /* Load Users */
 require_once('lib/ApimoApiUsers.php');
 $api = new Apimo_Api_Users();
-//$report = $api->update();
-//print_r($report);
+$report = $api->update();
+print_r($report);
 
 /* Load Properties */
 require_once('lib/ApimoApiProperties.php');
